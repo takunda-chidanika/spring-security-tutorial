2164b5049b7136164d4e98a7b6efde0cbfbe75e1
tjchidanika <tjchidanika@gmail.com>
2023-08-02 14:39:37 +0200

    Initial commit

---
+++ src/main/java/com/example/springsecuritytutorial/security/ApplicationSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.springsecuritytutorial.security;
+/**
+ *
+ * @author tjchidanika
+ * @created 2/8/2023
+ */
+ 
+public class ApplicationSecurityConfig {
+}
---
+++ src/main/java/com/example/springsecuritytutorial/student/StudentController.java
@@ -0,0 +1,9 @@
+package com.example.springsecuritytutorial.student;
+/**
+ *
+ * @author tjchidanika
+ * @created 2/8/2023
+ */
+ 
+public class StudentController {
+}
