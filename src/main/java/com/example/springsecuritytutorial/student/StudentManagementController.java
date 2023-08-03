256c64c2a16562f74851906eaab9610a5949e843
tjchidanika <tjchidanika@gmail.com>
2023-08-03 12:53:40 +0200

    Enable Form Based Authentication

--- src/main/java/com/example/springsecuritytutorial/student/StudentManagementController.java
+++ src/main/java/com/example/springsecuritytutorial/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritytutorial.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -20,21 +21,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student.toString());
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, Student student) {
         System.out.printf("%s %s%n", studentId, student.toString());
     }
